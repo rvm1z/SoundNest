f4fad8840064caec76cc60787f8b608971dff874
Ramiz <rr2856@ece.jgec.ac.in>
2025-06-15 19:35:55 +0530

    Create sound.js

---
+++ src/components/sound.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+const Sound = () => {
+  return (
+    <div>
+      <h2>Sound Component</h2>
+      <p>This is the placeholder for Sound</p>
+    </div>
+  );
+};
+
+export default Sound;
