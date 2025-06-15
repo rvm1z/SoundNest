bfd3cdc9c55507d76bef6c2bd3ced0649f43044c
Ramiz <rr2856@ece.jgec.ac.in>
2025-06-15 19:47:54 +0530

    Create AddSound.jsx

---
+++ src/components/AddSound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Sound from './Sound';
+
+const AddSound = () => {
+  return (
+    <div>
+      <h2>Add a Song</h2>
+      <Sound />
+    </div>
+  );
+};
+
+export default AddSound;
